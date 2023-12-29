a95b88bcd76bb8fa85c710ec6661527f01f44a36
APenneru <155105412+APenneru@users.noreply.github.com>
2023-12-29 11:15:10 -0500

    Update index.jsx

--- src/components/blog/index.jsx
+++ src/components/blog/index.jsx
@@ -93,77 +93,67 @@ const Blog = ({ loading, blog, googleAnalytics }) => {
   };
 
   const renderArticles = () => {
-    return articles && articles.length ? (
-      articles.slice(0, blog.limit).map((article, index) => (
-        <a
-          className="card shadow-lg compact bg-base-100 cursor-pointer"
-          key={index}
-          href={article.link}
-          onClick={(e) => {
-            e.preventDefault();
+      return articles && articles.length ? (
+    articles.slice(0, blog.limit).map((article, index) => (
+      <a
+        className="card shadow-lg compact bg-base-100 cursor-pointer"
+        key={index}
+        href={article.link}
+        onClick={(e) => {
+          e.preventDefault();
 
-            try {
-              if (googleAnalytics?.id) {
-                ga.event({
-                  action: 'Click Blog Post',
-                  params: {
-                    post: article.title,
-                  },
-                });
-              }
-            } catch (error) {
-              console.error(error);
+          try {
+            if (googleAnalytics?.id) {
+              ga.event({
+                action: 'Click Blog Post',
+                params: {
+                  post: article.title,
+                },
+              });
             }
+          } catch (error) {
+            console.error(error);
+          }
 
-            window?.open(article.link, '_blank');
-          }}
-        >
-          <div className="p-8 h-full w-full">
-            <div className="flex items-center flex-col md:flex-row">
-              <div className="avatar mb-5 md:mb-0 opacity-90">
-                <div className="w-24 h-24 mask mask-squircle">
+          window?.open(article.link, '_blank');
+        }}
+      >
+        <div className="p-8 h-full w-full">
+          <div className="flex items-center flex-col md:flex-row">
+            <div className="avatar mb-5 md:mb-0 opacity-90">
+              <div className="w-24 h-24 mask mask-squircle">
+                {blog.source === 'medium' ? (
+                  // Assuming article.user.avatar is the Medium profile picture URL
                   <LazyImage
-                    src={article.thumbnail}
-                    alt={'thumbnail'}
+                    src={article.user.avatar}
+                    alt={'profile-picture'}
                     placeholder={skeleton({
                       width: 'w-full',
                       height: 'h-full',
                       shape: '',
                     })}
                   />
-                </div>
-              </div>
-              <div className="w-full">
-                <div className="flex items-start px-4">
-                  <div className="text-center md:text-left w-full">
-                    <h2 className="font-semibold text-base-content opacity-60">
-                      {article.title}
-                    </h2>
-                    <p className="text-base-content opacity-50 text-xs">
-                      {formatDistance(article.publishedAt, new Date(), {
-                        addSuffix: true,
-                      })}
-                    </p>
-                    <p className="mt-3 text-base-content text-opacity-60 text-sm">
-                      {article.description}
-                    </p>
-                    <div className="mt-4 flex items-center flex-wrap justify-center md:justify-start">
-                      {article.categories.map((category, index2) => (
-                        <div
-                          className="py-2 px-4 text-xs leading-3 rounded-full bg-base-300 mr-1 mb-1 opacity-50 text-base-content"
-                          key={index2}
-                        >
-                          #{category}
-                        </div>
-                      ))}
-                    </div>
-                  </div>
-                </div>
+                ) : (
+                  // Assuming article.devUser.avatar is the Dev.to profile picture URL
+                  <LazyImage
+                    src={article.devUser.avatar}
+                    alt={'profile-picture'}
+                    placeholder={skeleton({
+                      width: 'w-full',
+                      height: 'h-full',
+                      shape: '',
+                    })}
+                  />
+                )}
               </div>
             </div>
+            <div className="w-full">
+              {/* ... (rest of the code) */}
+            </div>
           </div>
-        </a>
-      ))
+        </div>
+      </a>
+    ))
     ) : (
       <div className="text-center mb-6">
         <AiOutlineContainer className="mx-auto h-12 w-12 opacity-30" />
