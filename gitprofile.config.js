fb420eec0ed47138701589a3cb6d262c5818984b
APenneru <155105412+APenneru@users.noreply.github.com>
2023-12-29 10:58:10 -0500

    Update gitprofile.config.js

--- gitprofile.config.js
+++ gitprofile.config.js
@@ -89,7 +89,7 @@ const config = {
   blog: {
     source: 'medium', // medium | dev
     username: 'imjosef', // to hide blog section, keep it empty
-    limit: 1, // How many posts to display. Max is 10.
+    limit: 3, // How many posts to display. Max is 10.
   },
   googleAnalytics: {
     id: '', // GA3 tracking id/GA4 tag id UA-XXXXXXXXX-X | G-XXXXXXXXXX
