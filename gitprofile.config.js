bbeff3cc3022f53da2acc3968355f769157c1163
APenneru <155105412+APenneru@users.noreply.github.com>
2023-12-29 10:55:42 -0500

    Update gitprofile.config.js

--- gitprofile.config.js
+++ gitprofile.config.js
@@ -88,7 +88,7 @@ const config = {
   // Display blog posts from your medium or dev account. (Optional)
   blog: {
     source: 'medium', // medium | dev
-    username: 'Josef Cruz', // to hide blog section, keep it empty
+    username: 'imjosef', // to hide blog section, keep it empty
     limit: 1, // How many posts to display. Max is 10.
   },
   googleAnalytics: {
