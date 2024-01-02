6aae6d30dc04c9f8c487bbf93dc9a2ea799d88d0
APenneru <155105412+APenneru@users.noreply.github.com>
2024-01-01 19:08:42 -0500

    Update gitprofile.config.js

--- gitprofile.config.js
+++ gitprofile.config.js
@@ -30,7 +30,7 @@ const config = {
   },
   resume: {
     fileUrl:
-      'https://drive.google.com/file/d/16p_QzJmrRfN4TF61Qrbux_6VTg4O88f-/view?usp=drive_link', // Empty fileUrl will hide the `Download Resume` button.
+      'https://drive.google.com/file/d/16p_QzJmrRfN4TF61Qrbux_6VTg4O88f-/view?usp=sharing', // Empty fileUrl will hide the `Download Resume` button.
   },
   skills: [
     'Python',
